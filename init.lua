04ca43158c6f8a5ca89966968902bc824e698fc7
Dencer <tdaddon@163.com>
2019-04-11 14:38:40 +0800

    refactor: Added init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- init.lua
+-- @Author : DengSir (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 4/11/2019 2:14 PM
+
+require('lib.base.table')
+require('lib.base.string')
+require('lib.base.class')
